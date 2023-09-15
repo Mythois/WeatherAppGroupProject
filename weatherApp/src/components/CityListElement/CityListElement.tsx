a9b6e7c4ac10a0442af9a20a76c0ccc0312b186d
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2023-09-15 16:07:39 +0200

    make favorite checkbox on city list element

--- weatherApp/src/components/CityListElement/CityListElement.tsx
+++ weatherApp/src/components/CityListElement/CityListElement.tsx
@@ -1,20 +1,44 @@
-import React from 'react'
-import './CityListElement.css'
+
+// This is the CityListElement component. It displays the city name, weather icon, and temperature for a city. 
+// Users can also favorite the city. 
+
+import React, { useState } from 'react';
+import './CityListElement.css';
 
 interface CityListElementProps {
-  cityName: string
-  cityTemp: number
-  cityWeatherIcon: string
+  cityName: string;
+  cityTemp: number;
+  cityWeatherIcon: string;
 }
 
-function CityListElement({cityName, cityTemp, cityWeatherIcon}: CityListElementProps) {
+function CityListElement({
+  cityName,
+  cityTemp,
+  cityWeatherIcon,
+}: CityListElementProps) {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const toggleFavorite = () => {
+    setIsFavorite(!isFavorite);
+  };
+
   return (
     <div className='cityListElement'>
-        <p>{cityName}</p>
-        <p>{cityWeatherIcon}</p>
-        <p>{cityTemp}℃</p>
+      <p>{cityName}</p>
+      <p>{cityWeatherIcon}</p>
+      <p>{cityTemp}℃</p>
+
+      <label className='favorite-checkbox'>
+        <input
+          type='checkbox'
+          checked={isFavorite}
+          onChange={toggleFavorite}
+          className='hidden-checkbox'
+        />
+        <span className='star'>&#9733;</span>
+      </label>
     </div>
-  )
+  );
 }
 
-export default CityListElement
+export default CityListElement;

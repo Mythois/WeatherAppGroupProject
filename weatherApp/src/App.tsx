a07468afab6baa9a2a96b5e2dc76fb3fc991ac3e
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2023-09-15 13:20:27 +0200

    header in nav still not responsive for f.ex ipad

--- weatherApp/src/App.tsx
+++ weatherApp/src/App.tsx
@@ -6,7 +6,7 @@ import Footer from './components/Footer/Footer'
 
 
 
-import { Navbar } from './components/Navbar/Navbar'
+import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import CityView from './pages/CityView/CityView'
 import Home from './pages/Home/Home'

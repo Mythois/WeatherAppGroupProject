617e8637cd6166e13171e4ab503596afbeffe7d9
Ola Lømo Ellingsen <olalel@stud.ntnu.no>
2023-09-15 12:57:52 +0200

    fixed small issue with hamburger menu

--- weatherApp/src/components/Navbar/Navbar.tsx
+++ weatherApp/src/components/Navbar/Navbar.tsx
@@ -1,20 +1,27 @@
-import './Navbar.css'
-import { Link } from "react-router-dom"
-import React, {useState} from 'react'
+import React, { useState } from 'react';
+import './Navbar.css';
 
-export const Navbar = () => {
-    const [menuOpen, setmenuOpen] = useState(false)
-    return (<nav>
-        <div className={`menu ${menuOpen ? 'open' : ''}`} onClick={() => setmenuOpen(!menuOpen)}>
-            <span className={menuOpen ? 'close-icon1' : ''}></span>
-            <span className={menuOpen ? 'close-icon2' : ''}></span>
-            <span className={menuOpen ? 'close-icon3' : ''}></span>
-        </div>
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-        <div className='logoContainer'><h2>WebDevWeather</h2></div>
-        <ul className={menuOpen ? "open" :""}>
-            <li className="navigationElement"><Link to="/Home">Home</Link></li>
-            <li className="navigationElement"><Link to="/Favourites">Favourites</Link></li>
-        </ul>
-    </nav>)
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <nav>
+      <div className={`menu ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <div className="bar1"></div>
+        <div className="bar2"></div>
+        <div className="bar3"></div>
+      </div>
+      <div className='logoContainer'><h2>WebDevWeather</h2></div>
+      <ul className={menuOpen ? "open" : ""}>
+        <li className="navigationElement"><a href="/Home">Home</a></li>
+        <li className="navigationElement"><a href="/Favourites">Favourites</a></li>
+      </ul>
+    </nav>
+  );
 }
+
+export default Navbar;

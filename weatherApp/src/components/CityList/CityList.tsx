985d64365153c0c3bfaeec372b4d30a9e631832f
runarli <runarli@stud.ntnu.no>
2023-09-18 01:20:48 +0200

    [#12] Connect search function to API data
    Also fix the search so that the cityList updates based on the input

--- weatherApp/src/components/CityList/CityList.tsx
+++ weatherApp/src/components/CityList/CityList.tsx
@@ -1,43 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './CityList.css';
 import CityListElement from '../CityListElement/CityListElement';
 
-function CityList() {
-  const [cityData, setCityData] = useState([]);
+interface CityListProps {
+  weatherData: any;
+  filter: string;
+}
+
+function CityList({ filter, weatherData }: CityListProps) {
+  const cities = ['Oslo', 'Bergen', 'Stavanger', 'Trondheim', 'Stockholm', 'København', 'Helsinki', 'Berlin', 'Paris', 'London'];
+  const [suggestions, setSuggestions] = useState<string[]>(cities);
 
   useEffect(() => {
-    // Fetch data from the API
-    fetch('your_api_endpoint_here')
-      .then((response) => response.json())
-      .then((data) => {
-        // Update the state with the fetched data
-        setCityData(data);
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-      });
-  }, []); // The empty dependency array ensures this effect runs once on component mount
-
+    setSuggestions(
+      cities.filter((c) => c.toLowerCase().includes(filter.toLowerCase()))
+    );
+  }, [filter]);
+  
   return (
     <div className='cityList'>
+      {suggestions.map((cityName, index) => {
+        const cityWeather = weatherData.find((cityData: { city: string; }) => cityData.city === cityName);
+        const cityTemp = cityWeather?.weatherData?.hourly?.temperature_2m[0];  // The temperature is currently the first one in the array, fix this
 
-        {/* hard coded data for testing */}
-        <CityListElement cityName='test' cityTemp={12} cityWeatherIcon='⛈'/>
-        <CityListElement cityName='test' cityTemp={12} cityWeatherIcon='⛈'/>
-        <CityListElement cityName='test' cityTemp={12} cityWeatherIcon='⛈'/>
-      
-
-        {/* Loop over the cityData array and create a CityListElement for each city */} 
-      {cityData.map((city, index) => (
-        <CityListElement
-          key={index} // Make sure to provide a unique key for each element in the list
-          cityName={city.name}
-          cityTemp={city.temp}
-          cityWeatherIcon={city.weatherIcon}
-        />
-      ))}
+        return (
+          <CityListElement
+            key={index}
+            cityName={cityName} 
+            cityTemp={cityTemp}
+            cityWeatherIcon={'⛈'} // Example weather icon
+          />
+        );
+      })}
     </div>
   );
 }
 
+
 export default CityList;
+
+
--- weatherApp/src/components/CityListElement/CityListElement.tsx
+++ weatherApp/src/components/CityListElement/CityListElement.tsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import './CityListElement.css'
+import React from 'react';
+import './CityListElement.css';
 
 interface CityListElementProps {
-  cityName: string
-  cityTemp: number
-  cityWeatherIcon: string
+  cityName: string;
+  cityTemp: number;
+  cityWeatherIcon: string;
+  onClick?: () => void; // Make onClick optional
 }
 
-function CityListElement({cityName, cityTemp, cityWeatherIcon}: CityListElementProps) {
+function CityListElement({ cityName, cityTemp, cityWeatherIcon, onClick }: CityListElementProps) {
   return (
-    <div className='cityListElement'>
-        <p>{cityName}</p>
-        <p>{cityWeatherIcon}</p>
-        <p>{cityTemp}℃</p>
+    <div className='cityListElement' onClick={onClick}>
+      <p>{cityName}</p>
+      <p>{cityWeatherIcon}</p>
+      <p>{cityTemp}℃</p>
     </div>
-  )
+  );
 }
 
-export default CityListElement
+export default CityListElement;

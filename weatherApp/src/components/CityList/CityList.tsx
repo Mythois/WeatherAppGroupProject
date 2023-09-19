c90f56f3d7213cbc5781f796e7892eb7592536a5
runarli <runarli@stud.ntnu.no>
2023-09-19 23:38:37 +0200

    [#24] Add filtering and sessionStorage for country

--- weatherApp/src/components/CityList/CityList.tsx
+++ weatherApp/src/components/CityList/CityList.tsx
@@ -1,12 +1,14 @@
 import './CityList.css';
 import CityListElement from '../CityListElement/CityListElement';
+import cityCountries from '../CountryFilter/cityCountries';
 
 interface CityListProps {
   filter: string;
   showFavoritesOnly: boolean
+  selectedCountry: string | null;
 }
 
-function CityList({ filter, showFavoritesOnly }: CityListProps) {
+function CityList({ filter, showFavoritesOnly, selectedCountry }: CityListProps) {
   const cities = ['Oslo', 'Bergen', 'Stavanger', 'Trondheim', 'Stockholm', 'København', 'Helsinki', 'Berlin', 'Paris', 'London'];
   
   // Retrieve favorites from sessionStorage
@@ -21,19 +23,24 @@ function CityList({ filter, showFavoritesOnly }: CityListProps) {
       favorites.push(key);
     }
   }
-
   
-  // Filter cities based on favorites and filter text
-  const filteredCities = cities.filter(
-    (city) => {
-      if (showFavoritesOnly) {
-        return (favorites.includes(city))
-      } else {
-        return city.toLowerCase().includes(filter.toLowerCase())
-      }
+  // Function to get the country of a city
+  const getCountryForCity = (city: string) => {
+    return cityCountries[city] || '';
+  };
+  
+  // Filter cities based on favorites, filter text and selected country
+  const filteredCities = cities.filter((city) => {
+    const country = getCountryForCity(city);
+
+    if (showFavoritesOnly) {
+      return (favorites.includes(city))
+    } else if (selectedCountry === null) {
+      return city.toLowerCase().includes(filter.toLowerCase())
+    } else {
+      return city.toLowerCase().includes(filter.toLowerCase()) && country === selectedCountry;
     }
-      
-  );
+  });
   
   return (
     <div className='cityList'>
--- weatherApp/src/pages/Home/Home.tsx
+++ weatherApp/src/pages/Home/Home.tsx
@@ -1,15 +1,44 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './Home.css';
 import { Search } from '../../components/Search/Search';
 import CityList from '../../components/CityList/CityList';
+import CountryFilter from '../../components/CountryFilter/CountryFilter';
+import cityCountries from '../../components/CountryFilter/cityCountries';
 
 function Home() {
   const [filter, setFilter] = useState("");
+  const [selectedCountry, setSelectedCountry] = useState<string | null>(null);
+
+  // Load selected country filter from sessionStorage on component mount
+  useEffect(() => {
+    const storedSelectedCountry = sessionStorage.getItem('selectedCountry');
+    if (storedSelectedCountry !== null) {
+      setSelectedCountry(storedSelectedCountry);
+    }
+  }, []);
+
+  // Save selected country filter to sessionStorage whenever it changes
+  useEffect(() => {
+    // Sjekk om selectedCountry er null eller "All"
+    if (selectedCountry !== null && selectedCountry !== "All") {
+      sessionStorage.setItem('selectedCountry', selectedCountry);
+    } else {
+      sessionStorage.removeItem('selectedCountry'); // Fjern lagret verdi for "All"
+    }
+  }, [selectedCountry]);
+
+
+  const countries = Array.from(new Set(Object.values(cityCountries)));
 
   return (
     <div className='home'>
       <Search onFilter={(value) => setFilter(value)} />
-      <CityList filter={filter} showFavoritesOnly={false} />
+      <CountryFilter
+        countries={countries}
+        selectedCountry={selectedCountry}
+        onSelectCountry={(country) => setSelectedCountry(country)}
+      />
+      <CityList filter={filter} showFavoritesOnly={false} selectedCountry={selectedCountry} />
     </div>
   );
 }

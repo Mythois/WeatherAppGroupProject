6597bc7503df37fb84a88fd36134b6f5ee7217f6
runarli <runarli@stud.ntnu.no>
2023-09-19 15:32:29 +0200

    fix-routing

--- weatherApp/src/App.tsx
+++ weatherApp/src/App.tsx
@@ -1,14 +1,10 @@
 import { Fragment, useState } from 'react'
 import './App.css'
-
-
-
-
-
 import Navbar from './components/Navbar/Navbar'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useParams } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Favorites from './pages/Favorites/Favorites'
+import CityDetails from './pages/CityDetails/CityDetails'
 
 
 
@@ -21,9 +17,16 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/Home" element={<Home/>}/>
         <Route path="/Favourites" element={<Favorites/>}/>
+        <Route path="/city/:cityName" element={<CityDetailsWrapper/>}/>
       </Routes>
     </Fragment>
   ) 
+}
+
+function CityDetailsWrapper() {
+  const { cityName = '' } = useParams();
+
+  return <CityDetails cityName={cityName} />;
 }
 
 export default App

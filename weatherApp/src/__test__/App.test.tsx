2e058a5ca78d714cd28ae3c67989a37b1df5d8c6
hanneswi <hanneswi@stud.ntnu.no>
2023-09-20 17:24:24 +0200

    [#9] feat: Add test for Navbar, getWeatherIcon, modify README
    Add test for Navbar (check that the title is WebDevWeather), getWeatherIcon (Check that the correct images are provided by the getWeathericon method for specific inputs), and a few changes to the README file

--- weatherApp/src/__test__/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import {render, screen} from "@testing-library/react"
-import App from "../App"
-
-
-it("should have the title WebDevWeather", () =>{
-    render(<App/>);
-    const message = screen.queryByText(/WebDevHeader/i);
-    expect(message).toBeVisible()
-})
---
+++ weatherApp/src/test/Navbar.test.tsx
@@ -0,0 +1,11 @@
+import {render, screen} from "@testing-library/react"
+import Navbar from "../components/Navbar/Navbar";
+
+
+
+it("should have the title WebDevWeather", () =>{
+    render(<Navbar/>);
+    const message = screen.queryByText(/WebDevWeather/i);
+    console.log(message)
+    expect(message).toBeVisible()
+})
